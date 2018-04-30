3b48e99b0f3ac5666abdf4c2b8b21115a64da087
Simon Andreas Frimann Lund <slund@cnexlabs.com>
2018-04-30 15:06:15 +0200

    ...

--- bty/__init__.py
+++ bty/__init__.py
@@ -12,6 +12,7 @@ from flask import Flask, render_template, request
 from flask import config as fconfig
 
 REGEX_HWA = r".*(([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})).*"
+REGEX_LBL = r"^LABEL\s+(.*)$"
 
 MACHINE_ATTRS = [
     "hostname",
@@ -156,21 +157,104 @@ def hdrs(content=None, content_type=None):
         ('Content-Length', str(len(content)))
     ]
 
-def cfg_load(cfg_fpath):
-    """
-    Load config from os.path.dirname("SCRIPT_FILENAME")/bty.json
+def pxe_config(cfg, host):
+    """Returns the pxe-config for the given host"""
 
-    @returns config as dict on success, None otherwise
-    """
-
-    if not os.path.exists(cfg_fpath):
-        print("FAILED: !exist(cfg_fpath: %r)" % cfg_fpath)
+    tmpl = host.get("ptemplate")
+    if tmpl is None:
+        print("FAILED: host: %r, tmpl: %r" % (host, tmpl))
         return None
 
-    with open(cfg_fpath, "r") as cfg_fd:
-        return json.load(cfg_fd)
+    return render_template(tmpl, cfg=cfg, host=host)
 
-    return None
+def pxe_config_install(cfg, host, pxe_content, pxe_fname=None):
+    """Install the given pxe config"""
+
+    print("pxe_config_install")
+
+    if pxe_fname is None:
+        pxe_fname = "01-%s" % host["hwa"].replace(":", "-")
+        pxe_fname = pxe_fname.lower()
+
+    pxe_fpath = os.sep.join([cfg["pconfigs"]["root"], pxe_fname])
+
+    print("pxe_fpath: %r" % pxe_fpath)
+
+    with open(pxe_fpath, "w") as pxe_fd:
+        pxe_fd.write(pxe_content)
+
+def cfg_init_pconfigs(cfg):
+    """
+    Initialize PXE configs by scanning system
+    """
+
+    fnames = []
+
+    cfg["pconfigs"]["coll"] = {}
+    for fpath in glob.glob(os.sep.join([cfg["pconfigs"]["root"], "*"])):
+        fname = os.path.basename(fpath)
+        fnames.append(fname)
+
+        pcfg = copy.deepcopy(PCONFIG_STRUCT)
+        pcfg["fname"] = fname
+        pcfg["content"] = open(fpath).read()
+
+        cfg["pconfigs"]["coll"][fname] = pcfg
+
+    if "default" in fnames:     # There is a default config, we can exit
+        return
+
+    print("WARNING: PXE default config is missing, creating one")
+    host = copy.deepcopy(MACHINE_STRUCT)
+    host["hostname"] = "default"
+    host["image"] = cfg["images"]["default"]
+    host["ptemplate"] = cfg["ptemplates"]["default"]
+    host["plabel"] = "install"
+
+    pxe = pxe_config(cfg, host)
+    if pxe is None:
+        print("FAILED: pxe_config for host: %r" % host)
+        return
+
+    pxe_config_install(cfg, host, pxe, "default")
+
+def cfg_init_ptemplates(cfg, app):
+    """
+    Initialize PXE configs by scanning system
+    """
+
+    def annotate_labels(tmpl):
+        """Returns a list of LABELs from the given TMPL source"""
+
+        tmpl["labels"] = []
+        for line in tmpl["content"].splitlines():
+            match = re.match(bty.REGEX_LBL, line)
+            if match:
+                tmpl["labels"].append(match.group(1))
+
+        tmpl["labels"].sort()
+
+    jenv = app.jinja_env
+    lodr = jenv.loader
+
+    cfg["ptemplates"]["coll"] = {}
+    for fname in jenv.list_templates(extensions=cfg["ptemplates"]["exts"]):
+        fname = str(fname)
+        tmpl = copy.deepcopy(PTEMPLATE_STRUCT)
+
+        tmpl["fname"] = fname
+        tmpl["content"], fpath, _ = lodr.get_source(app.jinja_env, fname)
+        # TODO: parse LABELS
+
+        annotate_labels(tmpl)
+
+        cfg["ptemplates"]["coll"][fname] = tmpl
+
+        if not cfg["ptemplates"]["default"] and "skip" not in fname:
+            cfg["ptemplates"]["default"] = fname
+
+        if not cfg["ptemplates"]["default_skip"] and "skip" in fname:
+            cfg["ptemplates"]["default_skip"] = fname
 
 def cfg_init_images(cfg):
     """
@@ -189,72 +273,6 @@ def cfg_init_images(cfg):
         cfg["images"]["coll"].sort()
         cfg["images"]["default"] = cfg["images"]["coll"][0]
 
-def cfg_init_pconfigs(cfg):
-    """
-    Initialize PXE configs by scanning system
-    """
-
-    cfg["pconfigs"]["coll"] = {}
-    for fpath in glob.glob(os.sep.join([cfg["pconfigs"]["root"], "*"])):
-        fname = os.path.basename(fpath)
-
-        pcfg = copy.deepcopy(PCONFIG_STRUCT)
-        pcfg["fname"] = fname
-        pcfg["content"] = open(fpath).read()
-
-        cfg["pconfigs"]["coll"][fname] = pcfg
-
-def cfg_init_ptemplates(cfg):
-    """
-    Initialize PXE configs by scanning system
-    """
-
-    jenv = app.jinja_env
-    lodr = jenv.loader
-
-    cfg["ptemplates"]["coll"] = {}
-    for fname in jenv.list_templates(extensions=cfg["ptemplates"]["exts"]):
-        fname = str(fname)
-        tmpl = copy.deepcopy(PTEMPLATE_STRUCT)
-
-        tmpl["fname"] = fname
-        tmpl["content"], fpath, _ = lodr.get_source(app.jinja_env, fname)
-        # TODO: parse LABELS
-
-        cfg["ptemplates"]["coll"][fname] = tmpl
-
-        if not cfg["ptemplates"]["default"] and "skip" not in fname:
-            cfg["ptemplates"]["default"] = fname
-
-        if not cfg["ptemplates"]["default_skip"] and "skip" in fname:
-            cfg["ptemplates"]["default_skip"] = fname
-
-def cfg_init(cfg_fpath):
-    """
-    Load config from `cfg_fpath`, use default if it does not exist
-    """
-
-    cfg = cfg_load(CFG_FPATH)
-    if cfg:
-        return cfg
-
-    print("FAILED: loading configuration")
-    print("WARNING: using default config")
-    cfg = copy.deepcopy(CFG_DEFAULT)
-
-    # TODO: set some default paths...
-    # TODO: ... like set the ptemplate["root"] to jinjas search dir
-
-    cfg_init_images(cfg)
-    cfg_init_pconfigs(cfg)
-    cfg_init_ptemplates(cfg)
-
-    pprint.pprint(cfg)
-
-    #if not cfg_save(CFG_FPATH, cfg):
-    #    print("FAILED: configuration seems severely broken")
-
-    return cfg
 
 def cfg_save(cfg_fpath, cfg):
     """
@@ -272,40 +290,67 @@ def cfg_save(cfg_fpath, cfg):
 
     return True
 
+def cfg_load(cfg_fpath):
+    """
+    Load config from os.path.dirname("SCRIPT_FILENAME")/bty.json
 
-def pxe_config(cfg, host):
-    """Returns the pxe-config for the given host"""
+    @returns config as dict on success, None otherwise
+    """
 
-    tmpl = host.get("ptemplate")
-    if tmpl is None:
-        print("FAILED: host: %r, tmpl: %r" % (host, tmpl))
+    if not os.path.exists(cfg_fpath):
+        print("FAILED: !exist(cfg_fpath: %r)" % cfg_fpath)
         return None
 
-    return render_template(tmpl, cfg=cfg, host=host)
+    with open(cfg_fpath, "r") as cfg_fd:
+        return json.load(cfg_fd)
 
-def pxe_config_install(cfg, host, pxe):
-    """Install the given pxe config"""
+    return None
 
-    print("pxe_config_install")
+def cfg_init(cfg_fpath, app):
+    """
+    Load config from `cfg_fpath`, use default if it does not exist
+    """
 
-    pxe_fname = "01-%s" % host["hwa"].replace(":", "-")
-    pxe_fname = pxe_fname.lower()
-    pxe_fpath = os.sep.join([cfg["pconfigs"]["root"], pxe_fname])
+    if app is None:
+        print("FAILED: app: %r" % app)
+        return None
 
-    print("pxe_fpath: %r" % pxe_fpath)
+    cfg = cfg_load(CFG_FPATH)
+    if cfg:
+        return cfg
 
-    with open(pxe_fpath, "w") as pxe_fd:
-        pxe_fd.write(pxe)
+    print("FAILED: cfg_load(%r)" % CFG_FPATH)
 
-app = Flask(__name__)
-#app.config.from_object('websiteconfig')
+    # TODO: set some default paths...
+    # TODO: ... like set the ptemplate["root"] to jinjas search dir
 
-cfg = cfg_init(CFG_FPATH)
-if cfg is None:
-    print("FAILED: cannot obtain a configuration")
+    print("WARNING: initializing a best-effort configuration...")
+    cfg = copy.deepcopy(CFG_DEFAULT)
+    cfg_init_images(cfg)
+    cfg_init_ptemplates(cfg, app)
+    cfg_init_pconfigs(cfg)
+
+    pprint.pprint(cfg)
+
+    #if not cfg_save(CFG_FPATH, cfg):
+    #    print("FAILED: configuration seems severely broken")
+
+    return cfg
+
+APP = Flask(__name__)
+#APP.config.from_object('websiteconfig')
+if APP is None:
+    print("FAILED: cannot instantiate APP")
     exit(1)
 
-@app.route("/jazz")
+CFG = None
+with APP.app_context():
+    CFG = cfg_init(CFG_FPATH, APP)
+    if CFG is None:
+        print("FAILED: cannot obtain a configuration")
+        exit(1)
+
+@APP.route("/jazz")
 def app_slow():
     """This is the jazzy part"""
 
@@ -333,15 +378,15 @@ def bulk_refresh(form):
 
     return
 
-@app.route("/cfg")
+@APP.route("/cfg")
 def app_cfg():
     """Provide config as JSON"""
 
-    return json.dumps(cfg)
+    return json.dumps(CFG)
 
-@app.route("/", methods=["GET", "POST"])
+@APP.route("/", methods=["GET", "POST"])
 def app_cfg_ui():
-    """sdfsd"""
+    """Render UI"""
 
     if request.method == "POST":
 
@@ -355,10 +400,10 @@ def app_cfg_ui():
         else:
             print("Process SINGLE update change")
 
-    return render_template('ui_cfg.html', cfg=cfg)
+    return render_template('ui_cfg.html', cfg=CFG)
 
-@app.route("/pxe", methods=["GET"])
-@app.route("/pxe/<hwa>", methods=["GET"])
+@APP.route("/pxe", methods=["GET"])
+@APP.route("/pxe/<hwa>", methods=["GET"])
 def app_pxe(hwa=None):
     """sdfsd"""
 
@@ -367,15 +412,15 @@ def app_pxe(hwa=None):
         print("FAILED: hwa_to_host, hwa: %r" % hwa)
         return "", 404
 
-    pcfg = pxe_config(cfg, host)
+    pcfg = pxe_config(CFG, host)
     if pcfg is None:
         print("FAILED: pxe_config" % hwa)
         return "", 404
 
     return pcfg
 
-@app.route("/bootstrap.sh")
-@app.route("/bootstrap.sh/<hwa>")
+@APP.route("/bootstrap.sh")
+@APP.route("/bootstrap.sh/<hwa>")
 def app_bootstrap(hwa=None):
     """@returns bootstrap script for the host to run"""
 
@@ -384,26 +429,26 @@ def app_bootstrap(hwa=None):
         print("FAILED: hwa_to_host, hwa: %r" % hwa)
         return "", ""
 
-    host = cfg["machines"]["coll"].get(hwa)
+    host = CFG["machines"]["coll"].get(hwa)
     if host is None:
-        print("FAILED: machines: %r, host: %r" % (cfg["machines"], host))
+        print("FAILED: machines: %r, host: %r" % (CFG["machines"], host))
         return "", 404
 
     if not host["managed"]:
         return render_template("bootstrap_cancel.sh")
 
-    tmpls = cfg["ptemplates"]["coll"]   # Switch to a non-install PXE LABEL
+    tmpls = CFG["ptemplates"]["coll"]   # Switch to a non-install PXE LABEL
     ptemplate = host["ptemplate"]
     labels = [lbl for lbl in tmpls[ptemplate]["labels"] if "install" not in lbl]
     if labels:
         labels.sort()
         host["plabel"] = labels[0]
-        cfg_save(CFG_FPATH, cfg)
+        cfg_save(CFG_FPATH, CFG)
 
-    pxe = pxe_config(cfg, host)         # Create PXE config for host
+    pxe = pxe_config(CFG, host)         # Create PXE config for host
     try:
-        pxe_config_install(cfg, host, pxe)
+        pxe_config_install(CFG, host, pxe)
     except IOError as exc:
         print("FAILED: pxe_config_install, err: %r" % exc)
 
-    return render_template("bootstrap.sh", cfg=cfg, host=host)
+    return render_template("bootstrap.sh", cfg=CFG, host=host)
